da0ce796d55dce45bed7aceba69145413e087ee6
Arilson <arilson_piovezan@hotmail.com>
2016-11-28 19:30:11 -0200

    alterado para potencia

--- Carro.php
+++ Carro.php
@@ -7,33 +7,64 @@ class Carro
 	const MARCA = "AUDI";
 
 	public $cor;
-	private $motor = 1.0;
+        /**
+         * @var Motor 
+         */
+	private $motor;
 	private $porta = 4;
 	private $marcaVeiculo = "AUDI";
 	private $tanqueCombustivel = 0; 	
 
-
-	public function __construct($cor = "Branco")
+        /**
+         * 
+         * @param Motor $motor
+         * @param string $cor
+         */
+	public function __construct(Motor $motor, $cor = "Branco")
 	{
 		$this->tanqueCombustivel = 10;
 		$this->cor = $cor;	
+                $this->motor = $motor;
 	}
-
+        
+        /**
+         * Liga o motor
+         */
 	public function ligar()
 	{
 
 	}	
 	
+        /*
+         * Desliga o motor
+         */
 	public function desligar()
 	{
 
 	}
-
-	public function andar()
+       
+        /**
+         * Faz o carro andar
+         * @param type $torque
+         */
+	public function andar($torque)
 	{
-	
+             echo "Andou ".$torque."metros";
 	}
+        /**
+         * Envia aceleração ao motor
+         * @param int $valor Valor da aceleração informada
+         */
+        public function acelerar($valor)
+        {
+            $torque = $this->motor->acelerar($valor);
+            $this->andar($torque);
+        }    
 
+        /**
+         * Abasteçe o veículo
+         * @param int $valor Valor a ser colocado no tanque
+         */
 	public function abastecer($valor)
 	{
 		$this->tanqueCombustivel += $valor;
--- fabrica.php
+++ fabrica.php
@@ -1,18 +1,31 @@
+<pre>
 <?php
 
 require_once "Carro.php"; 
+require_once './Motor.php';
+
+$motor = new Motor();
+
+$veiculo1 = new Carro($motor, "Preto");
+$veiculo2 = clone $veiculo1;
+
+$veiculo1->cor = "Vermelho";
+$veiculo1->abastecer(25);
+
+$veiculo1->acelerar(40);
+
+var_dump($veiculo1);
+
+echo "Potencia do motor:".Motor::POTENCIA;
+
+echo "Motor :".Carro::MODELO;
 
 
 
-$veiculo1 = new Carro("Preto");
-$veiculo2 = new Carro();
 
-$veiculo2->abastecer(25);
-var_dump($veiculo1, $veiculo2);
-
-echo $veiculo2::MODELO."\n";
+/*echo $veiculo2::MODELO."\n";
 echo $veiculo2::MARCA."\n";
-
+*?
 
 
 
